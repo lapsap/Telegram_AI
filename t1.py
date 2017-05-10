601e75ab8db2478473b2b653918672b17eea5c5f
Chris Tan <hihitan@gmail.com>
2017-05-10 23:11:00 +0800

    small changes

--- t1.py
+++ t1.py
@@ -4,11 +4,23 @@ import datetime
 import telepot
 import weather
 import sys
+import psycopg2
 from wit import Wit
+
+def db_adduser(chat_id):
+    msg = "insert into user_id (telegram_id) values (%s)" %chat_id
+    try:
+        cur = conn.cursor()
+        cur.execute(msg)
+        conn.commit()
+    except :
+        #print("add into db fail {0}").format(e)
+        buff = 0
 
 def handle(msg):
     chat_id = msg['chat']['id']
     command = msg['text']
+    db_adduser(chat_id)
 
     print ('%s sent command: %s' % (chat_id,command))
 
@@ -22,8 +34,6 @@ def handle(msg):
     elif command == 'weather':
         ans = weather.weather_forecast("Hsinchu")
         bot.sendMessage(chat_id, ans)
-    elif command == '軒軒':
-        bot.sendMessage(chat_id, "欣欣")
     else :
         resp = wit_client.message(command)
         if 'intent' in resp['entities']:
@@ -44,7 +54,7 @@ def handle(msg):
 
 def wit_send(request, response):
     print(response['text'])
-token_file = open("token.txt","r")
+token_file = open("token.txt","r")  #file to store token
 tokens = token_file.read().split('\n')
 wit_actions = { 'send' : wit_send, }
 wit_token = tokens[0] 
@@ -52,6 +62,10 @@ wit_client = Wit(access_token=wit_token, actions=wit_actions)
 bot = telepot.Bot(tokens[1])
 bot.message_loop(handle)
 print ('I am listening ...')
+try:
+    conn = psycopg2.connect(database="telegram_ai", user = "telegram_ai", password = "iamchris", host = "127.0.0.1", port = "5432")
+except:
+    print("connect db error")
 
 while 1:
     now = datetime.datetime.now()
